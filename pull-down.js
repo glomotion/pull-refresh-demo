0ac423b9888f34adbdd3f3d759dcae0603975ff3
Tim Paul <timpaul@google.com>
2014-04-29 19:14:11 +1000

    tidying up code a little...

--- pull-down.js
+++ pull-down.js
@@ -1,74 +1,112 @@
 $(function(){
 
+    // $('#cards-listing > article').stacker();
+
+    // create & innit the left/right carousel object
+    var carousel = new Carousel("#carousel");
+    carousel.init();
+
+    // create & innit the pull-down to refresh object
+    var pulldown = new PullDown("#hitzone");
+    pulldown.init();
+
+
+});
+
+
+
+/**
+ * Vertical pull-to-refresh
+ * @param ev: the full event instance
+ */
+function PullDown(element) {
+    
+    var self = this;
+
+    // some misc global vars that can update (switches mostly)
+    var pullDownMode, translateAmount, lockedIn;
+    var offset = 0;
+
     // establish some fixed variables (basic settings)
-	var TIMEOUT_VAL = 200;
-	var BREAKPOINT = 70;
+    var TIMEOUT_VAL = 200;
+    var BREAKPOINT = 70;
     var LOCK_OFFSET = 150;
-	var TRANSLATE_RATIO = .4;
+    var TRANSLATE_RATIO = .4;
 
     // create the JQ dom objects that we'll be operated on
-	var $body = $('body');
-	var $hitzone = $('#hitzone');
+    var $element = $(element);
     var $pulldown = $('#pull-down');
-	
-    // some misc global vars that change (switches mostly)
-	var pullDownMode, translateAmount, lockedIn;
-    var offset = 0;
+    var $container = $element.parent();
     
-    // setup the event listeners
-    $hitzone.hammer().on('touch dragdown dragup release', function(ev) {
-        vertPullHandler(ev);  
-    });
-
-    // a simple cancel button
-    $('.cancel').hammer().on('tap', function() {
-        resetMode();
-    });
-	
     /**
-     * Vertical pull event handler
-     * @param ev: the full event instance
+     * initialise
      */
-    function vertPullHandler(ev) {
+    this.init = function() {
 
-    	// stop browser scrolling
-    	// fixes much flakyness on Clank...
+        // a simple cancel button
+        $('.cancel').hammer().on('tap', function() {
+            self.resetMode();
+        });
+        
+        $element.hammer().on('touch dragdown dragup release', function(ev) {
+            self.vertPullHandler(ev);  
+        });
+
+        // $(window).on("load resize orientationchange", function() {
+        // });
+    };
+
+    this.vertPullHandler = function(ev) {
+
+        // stop browser scrolling
+        // fixes much flakyness on Clank...
         ev.gesture.preventDefault();
         
         switch(ev.type) {
 
             // on release we check how far we dragged + reset if no mode change is present
             case 'release':
-                $body.addClass('return');
-                $hitzone.css({
-			    	"transform" : "translate3d(0,0,0)"
-			    });
-			    if (pullDownMode) {
+                // 'return' class is used for css animation smoothness
+                $container.addClass('return');
+                // reset all translation values first up...
+                $element.css({
+                    "transform" : "translate3d(0,0,0)"
+                });
+                // if pullDownMode has been triggered, change into 'pull-down' mode
+                if (pullDownMode) {
                     $pulldown.attr('data-percent','100');
-                	$body.addClass('pulldown-mode');
-                    $hitzone.css({
+                    $container.addClass('pulldown-mode');
+                    $element.css({
                         "transform" : "translate3d(0," + LOCK_OFFSET + "px,0)"
                     });
                     lockedIn = true;
                 } else {
-                    resetMode();
+                    // otherwise, reset the mode
+                    self.resetMode();
                 }
-			    setTimeout(function() {
-			        $body.removeClass('return');
-			    }, TIMEOUT_VAL);
+                // once css transitions are complete, remove the class that enables them.
+                setTimeout(function() {
+                    $container.removeClass('return');
+                }, TIMEOUT_VAL);
                 
                 break;
 
 
             // when we dragdown
             case 'dragdown':
-            	translateAmount = ev.gesture.deltaY * TRANSLATE_RATIO;
+                // capture the basic gesture movement, and refine it for use in the UI
+                translateAmount = ev.gesture.deltaY * TRANSLATE_RATIO;
+                // have we dragged enough already to trigger 'pull-down' mode?
                 if (translateAmount >= BREAKPOINT) {
+                    // trigger 'pull-down' mode
                     pullDownMode = true;
                 }
+                // are we already in 'pull-down' mode?
+                // if so, we need to alter the 'translateAmount' a little...
                 if (lockedIn) {
                     // temporarily disable pulldown mode and allow dragging
-                    $body.removeClass('pulldown-mode');
+                    $container.removeClass('pulldown-mode');
+                    // global offset value ensures that we start translating from where we are supposed to
                     if (offset === 0) {
                         offset = LOCK_OFFSET;
                     }
@@ -77,13 +115,15 @@ $(function(){
                         pullDownMode = true;
                     }
                 } else {
+                    // we're not in 'pull-down' mode, so animate the distance left until the 'breakpoint'
                     var percentComplete = (100 / BREAKPOINT) * translateAmount;
                     if (percentComplete <= 101 && percentComplete > 0) {
-                        // animate thru the percentage levels
+                        // animate thru the percentage levels, in increments of 10
                         $pulldown.attr('data-percent',Math.round(percentComplete / 10) * 10);
                     }
                 }
-                $hitzone.css({
+                // finally, apply calculated 'translationAmount', as a 3d transform (butter smooth)...
+                $element.css({
                     "transform" : "translate3d(0," + translateAmount + "px,0)"
                 });
                 break;
@@ -97,44 +137,44 @@ $(function(){
                 }
                 if (lockedIn) {
                     // temporarily disable pulldown mode and allow dragging
-                    $body.removeClass('pulldown-mode');
+                    $container.removeClass('pulldown-mode');
                     translateAmount = ev.gesture.deltaY * TRANSLATE_RATIO + LOCK_OFFSET;
                 } else {
+                    // calculate current drag progress as a percentage
                     var percentComplete = (101 / BREAKPOINT) * translateAmount;
                     if (percentComplete <= 100 && percentComplete > 0) {
                         // animate thru the animation
                         $pulldown.attr('data-percent',Math.round(percentComplete / 10) * 10);
                     }
                 }
+                // final check to see if we have dragged up enough to cancel 'pull-down' mode
                 if (translateAmount <= BREAKPOINT) {
                     pullDownMode = false;
                 }
-                $hitzone.css({
+                // finally, apply calculated 'translationAmount', as a 3d transform (butter smooth)...
+                $element.css({
                     "transform" : "translate3d(0," + translateAmount + "px,0)"
                 });
                 break;
         }
+    };
 
-    }
-
-    function resetMode() {
-    	$hitzone.css({
+    // generic reset function, clears 'pull-down' mode, and restores the app to normal
+    this.resetMode = function() {
+        $element.css({
             "transform" : "translate3d(0,0,0)"
         });
-    	$body.attr('class','return');
-    	pullDownMode = false;
+        $container.removeClass('pulldown-mode').addClass('return');
+        pullDownMode = false;
         lockedIn = false;
         offset = 0;
         $pulldown.attr('data-percent','0');
         setTimeout(function() {
-    	    $body.removeClass('return');
-    	}, TIMEOUT_VAL);
-    }
+            $container.removeClass('return');
+        }, TIMEOUT_VAL);
+    };
+}
 
-    var carousel = new Carousel("#carousel");
-    carousel.init();
-
-});
 
 /**
 * super simple carousel
@@ -143,38 +183,41 @@ $(function(){
 function Carousel(element) {
     
     var self = this;
-    element = $(element);
 
-    var container = $(">ul", element);
-    var panes = $(">ul>li", element);
+    var $element = $(element);
+    var $container = $(">ul", $element);
+    var $panes = $(">ul>li", $element);
 
     var pane_width = 0;
-    var pane_count = panes.length;
+    var pane_count = $panes.length;
 
     var current_pane = 0;
-
 
     /**
      * initial
      */
     this.init = function() {
-        setPaneDimensions();
+        
+        this.setPaneDimensions();
+
+        $element.hammer().on('release dragleft dragright swipeleft swiperight', function(ev) {
+            self.horizPullHandler(ev);  
+        });
 
         $(window).on("load resize orientationchange", function() {
-            setPaneDimensions();
+            self.setPaneDimensions();
         });
     };
-
 
     /**
      * set the pane dimensions and scale the container
      */
-    function setPaneDimensions() {
-        pane_width = element.width();
-        panes.each(function() {
+    this.setPaneDimensions = function() {
+        pane_width = $element.width();
+        $panes.each(function() {
             $(this).width(pane_width);
         });
-        container.width(pane_width*pane_count);
+        $container.width(pane_width*pane_count);
     };
 
 
@@ -187,18 +230,18 @@ function Carousel(element) {
         current_pane = index;
 
         var offset = -((100/pane_count)*current_pane);
-        setContainerOffset(offset, animate);
+        this.setContainerOffset(offset, animate);
     };
 
 
-    function setContainerOffset(percent, animate) {
-        container.removeClass("animate");
+    this.setContainerOffset = function(percent, animate) {
+        $container.removeClass("animate");
 
         if(animate) {
-            container.addClass("animate");
+            $container.addClass("animate");
         }
 
-        container.css({
+        $container.css({
             "transform" : "translate3d("+ percent +"%,0,0)"
         });
     }
@@ -206,8 +249,7 @@ function Carousel(element) {
     this.next = function() { return this.showPane(current_pane+1, true); };
     this.prev = function() { return this.showPane(current_pane-1, true); };
 
-
-    function horizPullHandler(ev) {
+    this.horizPullHandler = function(ev) {
 
         // disable browser scrolling
         ev.gesture.preventDefault();
@@ -225,7 +267,7 @@ function Carousel(element) {
                     drag_offset *= .4;
                 }
 
-                setContainerOffset(drag_offset + pane_offset);
+                self.setContainerOffset(drag_offset + pane_offset);
                 break;
 
             case 'swipeleft':
@@ -253,8 +295,6 @@ function Carousel(element) {
                 break;
         }
     }
-
-    new Hammer(element[0], { dragLockToAxis: true }).on("release dragleft dragright swipeleft swiperight", horizPullHandler);
 }
 
 
